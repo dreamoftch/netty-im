4e11267f2dde61f7ffcc8fe9cb27f0b65eb92e2e
tianch <tianch@lieluobo.com>
2019-09-16 15:01:38 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # my-netty-im
+
+### 启动server
+
+`./gradlew server:run`
+
+### 启动netty-client
+
+`./gradlew client:run --args='userId'`
+
+userId表示用户名
+
+### 启动websocket-client
+
+直接使用浏览器访问client模块下resources/websocket/websocket-test.html即可
+
+然后就可以聊天了
--- client/src/main/kotlin/com/llb/test/im/client/IMClient.kt
+++ client/src/main/kotlin/com/llb/test/im/client/IMClient.kt
@@ -11,78 +11,81 @@ import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioSocketChannel
 import io.netty.handler.logging.LogLevel
 import io.netty.handler.logging.LoggingHandler
+import org.slf4j.LoggerFactory
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.*
 import kotlin.concurrent.thread
+
+private val logger = LoggerFactory.getLogger("IMClient")
+
+fun main(args: Array<String>) {
+    val userId = args.firstOrNull() ?: UUID.randomUUID().toString()
+    logger.info("userId is $userId")
+    IMClient().start(userId)
+}
 
 class IMClient {
 
-    companion object {
+    private var channel: Channel? = null
 
-        private var channel: Channel? = null
+    private var workerGroup: NioEventLoopGroup? = null
 
-        private lateinit var workerGroup: NioEventLoopGroup
-
-        @JvmStatic
-        fun main(args: Array<String>) {
-            try {
-                println("请输入您的大名:")
-                val reader = BufferedReader(InputStreamReader(System.`in`))
-                val userId = reader.readLine()
-                channel = connect(
-                    userId
-                ).channel()
-                readAndSendUserInputBackend(userId, reader)
-                channel?.closeFuture()?.sync()
-            } finally {
-                workerGroup.shutdownGracefully()
-            }
+    fun start(userId: String) {
+        try {
+            val reader = BufferedReader(InputStreamReader(System.`in`))
+            channel = connect(
+                userId
+            ).channel()
+            readAndSendUserInputBackend(userId, reader)
+            channel?.closeFuture()?.sync()
+        } finally {
+            workerGroup?.shutdownGracefully()
         }
+    }
 
-        private fun readAndSendUserInputBackend(userId: String, reader: BufferedReader) {
-            thread {
-                while (true) {
-                    println("请输入对方的大名(直接回车表示群聊):")
-                    val targetUserId = reader.readLine()
-                    println("请输入聊天内容:")
-                    val content = reader.readLine()
-                    val msg = Message().apply {
-                        this.sourceUserId = userId
-                        this.targetUserId = targetUserId
-                        this.messageContent = content
-                    }
-                    getChannel(userId).writeAndFlush(msg)
+    private fun readAndSendUserInputBackend(userId: String, reader: BufferedReader) {
+        thread {
+            while (true) {
+                logger.info("请输入对方的大名(直接回车表示群聊):")
+                val targetUserId = reader.readLine()
+                logger.info("请输入聊天内容:")
+                val content = reader.readLine()
+                val msg = Message().apply {
+                    this.sourceUserId = userId
+                    this.targetUserId = targetUserId
+                    this.messageContent = content
                 }
+                getChannel(userId).writeAndFlush(msg)
             }
         }
+    }
 
-        private fun getChannel(userId: String): Channel {
-            val currentChannel = channel
-            if (currentChannel == null || !channelAvailable(
-                    currentChannel
-                )
-            ) {
-                return connect(userId).channel()
-            }
-            return currentChannel
+    private fun getChannel(userId: String): Channel {
+        val currentChannel = channel
+        if (currentChannel == null || !channelAvailable(
+                currentChannel
+            )
+        ) {
+            return connect(userId).channel()
         }
+        return currentChannel
+    }
 
-        private fun connect(userId: String): ChannelFuture {
-            workerGroup = NioEventLoopGroup()
-            return Bootstrap()
-                .group(workerGroup)
-                .handler(LoggingHandler(LogLevel.INFO))
-                .channel(NioSocketChannel::class.java)
-                .option(ChannelOption.SO_KEEPALIVE, true)
-                .handler(ClientChannelInitializer(userId))
-                .connect("localhost", SERVER_PORT)
-                .sync()
-        }
+    private fun connect(userId: String): ChannelFuture {
+        workerGroup = NioEventLoopGroup()
+        return Bootstrap()
+            .group(workerGroup)
+            .handler(LoggingHandler(LogLevel.INFO))
+            .channel(NioSocketChannel::class.java)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .handler(ClientChannelInitializer(userId))
+            .connect("localhost", SERVER_PORT)
+            .sync()
+    }
 
-        private fun channelAvailable(channel: Channel): Boolean {
-            return channel.isActive && channel.isOpen
-        }
-
+    private fun channelAvailable(channel: Channel): Boolean {
+        return channel.isActive && channel.isOpen
     }
 
 }
